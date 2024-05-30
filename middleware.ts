b72a62d017488ab37e417a8a03fdab984f369737
ToddHCarpenter <thueycarp@gmail.com>
2024-05-30 11:20:39 -0400

    Created Clerk Webhook

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware();
+// Make sure that the `/api/webhooks/(.*)` route is not protected here
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
 
 // import { clerkMiddleware } from "@clerk/nextjs/server";
